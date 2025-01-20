3706380bf843515095db38fa196b0be58aba028b
JisnaP <jisna12@gmail.com>
2025-01-20 20:33:19 +0300

    changes updated

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 from typing import List
-'''def get_requiremet(file_path:str)->List[str]:
+HYPEN_E_DOT='-e .'
+def get_requirement(file_path:str)->List[str]:
     requirements = []
     with open(file_path) as f:
         requirements=f.readlines()
@@ -8,18 +9,18 @@ from typing import List
         
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-    return requirements'''
+    return requirements
 
      
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.4"
-REPO_NAME = "mongodbconnectorpkg"
+__version__ = "0.0.8"
+REPO_NAME = "Mongo_DB_Connector_PYPI_Package"
 PKG_NAME= "databaseautomation"
-AUTHOR_USER_NAME = "sunnysavita10"
-AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
+AUTHOR_USER_NAME = "JisnaP"
+AUTHOR_EMAIL = "jisna12@gmail.com"
 
 setup(
     name=PKG_NAME,
@@ -35,4 +36,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    install_requires=get_requirement("./requirements.txt")
     )
